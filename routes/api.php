4cc9ef13216b19cebf37a08eb1f71fdfa785aa27
alihamzehei <alihamzehei2017@gmail.com>
2024-02-29 15:11:22 +0330

    feat:updated

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->middleware('auth.basic')->group(function () {
+Route::prefix('v1/labyrinth')->middleware('auth.basic')->group(function () {
     Route::controller(LabyrinthController::class)->group(function () {
-        Route::get('labyrinth', 'index');
-        Route::post('labyrinth', 'store');
-        Route::get('labyrinth/{labyrinth}', 'show');
-        Route::put('labyrinth/{labyrinth}/playfield/{x}/{y}/{type}', 'updatePlayfield');
-        Route::put('labyrinth/{labyrinth}/start/{x}/{y}', 'setStart');
-        Route::put('labyrinth/{labyrinth}/end/{x}/{y}', 'setEnd');
-        Route::get('labyrinth/{labyrinth}/solution', 'solution');
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{labyrinth}', 'show');
+        Route::put('/{labyrinth}/playfield/{x}/{y}/{type}', 'updatePlayfield');
+        Route::put('/{labyrinth}/start/{x}/{y}', 'setStart');
+        Route::put('/{labyrinth}/end/{x}/{y}', 'setEnd');
+        Route::get('/{labyrinth}/solution', 'solution');
     });
 });
